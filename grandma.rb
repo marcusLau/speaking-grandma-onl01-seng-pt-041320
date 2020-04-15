626deef6ea6a1e6f3fc38d810aa97aed04d36308
Marcus Lau <lau.marcus76@gmail.com>
2020-04-15 16:55:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 def speak_to_grandma(phrase)
   if (phrase == "I LOVE YOU GRANDMA!")
     # binding.pry
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif (phrase == phrase.upcase)
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else 
-    puts "HUH?! SPEAK UP SONNY!"
+    "HUH?! SPEAK UP SONNY!"
   end
 end
   
